6067dd1c48a3a306ad2625dc9258e8ab98a623a7
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-06-29 19:42:31 -0300

    udpate

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 locals {
-  name_prefix = substr("${var.cluster_name}-alb", 0, 32)
+  name_prefix = substr("eks-alb-controller-${var.cluster_name}", 0, 32)
 }
 
-resource "aws_iam_policy" "aws_AWSLoadBalancerController" {
+resource "aws_iam_policy" "alb" {
   name        = local.name_prefix
   description = "AWSLoadBalancerController policy for EKS cluster ${var.cluster_name}"
   policy      = file("${path.module}/policy.json")
 }
 
 
-module "irsa_aws_AWSLoadBalancerController" {
+module "irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 4.2"
 
   create_role                   = true
   role_name                     = local.name_prefix
   provider_url                  = var.cluster_oidc_issuer_url
-  role_policy_arns              = [aws_iam_policy.aws_AWSLoadBalancerController.arn]
+  role_policy_arns              = [aws_iam_policy.alb.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"]
 }
