712755f5c6d24f09fd04f07139d3fbf0f9a42511
Adriano Ferraz <adrianohf@gmail.com>
2022-09-16 11:07:49 -0300

    Changed service_account_name to aws-load-balancer-controller

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "service_account_namespace" {
 
 variable "service_account_name" {
   description = "ServiceAccount name for alb controller"
-  default     = "alb"
+  default     = "aws-load-balancer-controller"
 }
 variable "tags" {
   description = "AWS tags to apply to resources"
